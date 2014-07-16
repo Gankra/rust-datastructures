1767d1a9d85d7d0392066474f99c920ec790c92c
Alexis Beingessner <a.beingessner@gmail.com>
2014-07-15 23:44:48 -0400

    restructuring project into one source dir

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+extern crate coltests;
+
+pub mod bst;
+pub mod splaytree;
+pub mod binomialheap;
+pub mod pairingheap;
+pub mod binaryheap;
--- src/test/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#![feature(macro_rules)]
-#[macro_escape]
-
-pub mod utils;
-pub mod collection;
-pub mod map;
