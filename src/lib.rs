b1468e13393d5918ae4c9dd24d819b42fa72af66
Alexis Beingessner <a.beingessner@gmail.com>
2014-08-20 10:22:51 -0400

    clarify purpose

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,11 @@
 #![feature(unsafe_destructor)]
 #![feature(default_type_params)]
 
-//! This is a test
+//! A test bed for experimenting with data structures in Rust.
+//!
+//! Publicly exported structures are usable, although perhaps
+//! impractical. However if they do not have tests, they are probably
+//! unfinished, and almost certainly incorrect.
 
 #[phase(plugin, link)]
 extern crate coltests;
